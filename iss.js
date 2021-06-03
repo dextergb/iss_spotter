5641e082168c84f8e76b28adeaf23f6f48a4cd6f
dextergb <dextergb@rogers.com>
2021-06-03 22:16:05 +0000

    implemented fetchCoordsByIP function

--- iss.js
+++ iss.js
@@ -24,4 +24,29 @@ const fetchMyIP = function (callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function (ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(
+        Error(
+          `Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`
+        ),
+        null
+      );
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+// Don't need to export the other function since we are not testing it right now.
+module.exports = { fetchCoordsByIP };
+module.exports = { fetchMyIP, fetchCoordsByIP };
