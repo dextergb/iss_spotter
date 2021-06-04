5610b28dda465ee7019f8a7f8bcb3ce4e0a77456
dextergb <dextergb@rogers.com>
2021-06-04 04:29:27 +0000

    implement promises callback version

--- index2.js
+++ index2.js
@@ -1,6 +1,18 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require("./iss_promised");
 
-fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then(body => console.log(body));
+const printPassTimes = function (passTimes) {
+  for (const pass of passTimes) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${dateTime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("An ERROR occurred", error.message);
+  });
--- iss_promised.js
+++ iss_promised.js
@@ -9,12 +9,12 @@ const fetchMyIP = function () {
   return request("https://api.ipify.org?format=json");
 };
 
-/* 
+/*
  * Makes a request to freegeoip.app using the provided IP address, to get its geographical information (latitude/longitude)
  * Input: JSON string containing the IP address
  * Returns: Promise of request for lat/lon
  */
-const fetchCoordsByIP = function(body) {
+const fetchCoordsByIP = function (body) {
   const ip = JSON.parse(body).ip;
   return request(`https://freegeoip.app/json/${ip}`);
 };
@@ -24,10 +24,24 @@ const fetchCoordsByIP = function(body) {
  * Input: JSON body containing geo data response from freegeoip.app
  * Returns: Promise of request for fly over data, returned as JSON string
  */
-const fetchISSFlyOverTimes = function(body) {
+const fetchISSFlyOverTimes = function (body) {
   const { latitude, longitude } = JSON.parse(body);
   const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
   return request(url);
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+/*
+ * Input: None
+ * Returns: Promise for fly over data for users location
+ */
+const nextISSTimesForMyLocation = function () {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
